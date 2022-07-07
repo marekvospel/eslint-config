dffef046a901408836a4a450b3cc45e76240cddd
Marek Vospěl <marek@vospel.cz>
2022-07-07 12:54:59 +0200

    feat: use spread operator instead of Object.assign, minify configs, add banners

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 
 import { resolve } from 'path'
+import esbuild from 'rollup-plugin-esbuild'
 
 if (!process.env.TARGET) {
   throw new Error('TARGET package must be specified via --environment flag.')
@@ -9,10 +10,19 @@ const packagesDir = resolve(__dirname, 'packages')
 const packageDir = resolve(packagesDir, process.env.TARGET)
 const pkg = require(resolve(packageDir, 'package.json'))
 
+const banner = `/*!
+ * ${ pkg.name } v${ pkg.version }
+ * (c) ${ new Date().getFullYear() } ${ pkg.author }
+ */`
+
 module.exports = {
   input: 'src/.eslintrc.package.js',
+  plugins: [
+    esbuild({ minify: true }),
+  ],
   output: {
     file: pkg.main,
+    banner,
     format: 'cjs',
   },
 }
